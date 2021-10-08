b0ab299d91753950931ff74b938d9cad27726845
Neng <xiad0001@ntu.edu.sg>
2021-10-08 20:37:23 +0800

    Add eyeHear script

---
+++ eye_hear/main.py
@@ -0,0 +1,416 @@
+#!/usr/bin/env python
+
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Google Cloud Speech API sample application using the streaming API.
+NOTE: This module requires the dependencies `pyaudio` and `termcolor`.
+To install using pip:
+    pip install pyaudio
+    pip install termcolor
+Example usage:
+    python transcribe_streaming_infinite.py
+"""
+
+# [START speech_transcribe_infinite_streaming]
+
+import re
+import sys
+import time
+
+from google.cloud import speech
+import pyaudio
+from six.moves import queue
+
+
+##Saad salvage.garden 16sep: added OLED display support
+##Saad last update: 19sep - displayText for SST transcription output
+
+# SPDX-FileCopyrightText: Tony DiCola
+# SPDX-License-Identifier: CC0-1.0
+
+# Basic example of clearing and drawing pixels on a SSD1306 OLED display.
+# This example and library is meant to work with Adafruit CircuitPython API.
+
+# Import all board pins.
+from board import SCL, SDA
+import busio
+
+# Import the SSD1306 module.
+import adafruit_ssd1306
+
+
+# Create the I2C interface.
+i2c = busio.I2C(SCL, SDA)
+
+from PIL import Image, ImageDraw, ImageFont
+
+# Setting some variables for our reset pin etc.
+#RESET_PIN = digitalio.DigitalInOut(board.D4)
+# Very important... This lets py-gaugette 'know' what pins to use in order to reset the display
+
+# Create the SSD1306 OLED class.
+# The first two parameters are the pixel width and pixel height.  Change these
+# to the right size for your display!
+display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+# Alternatively you can change the I2C address of the device with an addr parameter:
+# display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x31)
+
+# Clear the display.  Always call show after changing pixels to make the display
+# update visible!
+display.fill(0)
+display.show()
+
+
+# Audio recording parameters
+STREAMING_LIMIT = 240000  # 4 minutes
+SAMPLE_RATE = 16000
+CHUNK_SIZE = int(SAMPLE_RATE / 10)  # 100ms
+
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+YELLOW = "\033[0;33m"
+SPINNER =  ["⠁","⠂","⠄","⡀","⢀","⠠","⠐","⠈"] #listener indicator = 8 frames
+x = 0
+
+# OLED Display INIT 
+
+# Create blank image for drawing.
+image = Image.new("1", (display.width, display.height))
+draw = ImageDraw.Draw(image)
+
+# Load a font in 2 different sizes.
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 10) # size 10 gives us 26 characters per screen
+tinyFont = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 8) #size 8 gives us 30 characters per screen
+
+header = 10 #space for the first row / status bar / yellow region of OLED
+
+print("OLED Display test")
+display.fill(0)
+w_delta = display.width / 10
+h_delta = display.height / 10
+for i in range(4):
+    display.rect(0, 0, int(w_delta * i), int(h_delta * i), 1)
+display.show()
+time.sleep(0.1)
+
+print("OLED Display text test")
+try:
+	# Clear display.
+	display.fill(0)
+	display.show()
+
+	# Draw the text
+	draw.text((0, 0), "EyeHear activated!", font=tinyFont, fill=255)
+	display.image(image)
+	display.show()
+	time.sleep(1)
+except Error:
+	print("Display test failed ")
+	for i in range(11):
+    		display.rect(0, 0, int(w_delta * i), int(h_delta * i), 1)
+	display.show()
+	print(Error)
+
+time.sleep(0.1)
+
+def displayRedText(oledText):
+#overlay text on top row before it goes green
+	draw = ImageDraw.Draw(image)
+	draw.rectangle((0, 0, display.width, header), outline=0, fill=0)
+	draw.text((0,0), oledText, font=tinyFont, fill=255)
+	display.image(image)
+	display.show()
+	#print("~") #debug oled
+	time.sleep(0.1)
+
+
+def displayText(oledText):
+#	image = Image.new("1", (display.width, display.height))
+	#fill empty recntangle to clear the screen
+	draw = ImageDraw.Draw(image)
+	draw.rectangle((0, header, display.width -1, display.height -1), outline=1, fill=0)
+	draw.text((0,header), oledText, font=font, fill=255)
+	display.image(image)
+	display.show()
+#	print(".") #debug oled
+	time.sleep(0.1)
+
+def spinSPinner():
+        x+=1
+        if x>7 : x=0
+        draw = ImageDraw.Draw(image)
+        draw.rectangle((0, 0, 4, header), outline=0, fill=0)
+        draw.text((0,0), SPINNER[x], font=tinyFont, fill=255)
+        display.image(image)
+        display.show()
+
+def get_current_time():
+    """Return Current Time in MS."""
+
+    return int(round(time.time() * 1000))
+
+
+class ResumableMicrophoneStream:
+    """Opens a recording stream as a generator yielding the audio chunks."""
+
+    def __init__(self, rate, chunk_size):
+        self._rate = rate
+        self.chunk_size = chunk_size
+        self._num_channels = 1
+        self._buff = queue.Queue()
+        self.closed = True
+        self.start_time = get_current_time()
+        self.restart_counter = 0
+        self.audio_input = []
+        self.last_audio_input = []
+        self.result_end_time = 0
+        self.is_final_end_time = 0
+        self.final_request_end_time = 0
+        self.bridging_offset = 0
+        self.last_transcript_was_final = False
+        self.new_stream = True
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            channels=self._num_channels,
+            rate=self._rate,
+            input=True,
+            frames_per_buffer=self.chunk_size,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
+            stream_callback=self._fill_buffer,
+        )
+
+    def __enter__(self):
+
+        self.closed = False
+        return self
+
+    def __exit__(self, type, value, traceback):
+
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        # Signal the generator to terminate so that the client's
+        # streaming_recognize method will not block the process termination.
+        self._buff.put(None)
+        self._audio_interface.terminate()
+
+    def _fill_buffer(self, in_data, *args, **kwargs):
+        """Continuously collect data from the audio stream, into the buffer."""
+
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def generator(self):
+        """Stream Audio from microphone to API and to local buffer"""
+
+        while not self.closed:
+            data = []
+
+            if self.new_stream and self.last_audio_input:
+
+                chunk_time = STREAMING_LIMIT / len(self.last_audio_input)
+
+                if chunk_time != 0:
+
+                    if self.bridging_offset < 0:
+                        self.bridging_offset = 0
+
+                    if self.bridging_offset > self.final_request_end_time:
+                        self.bridging_offset = self.final_request_end_time
+
+                    chunks_from_ms = round(
+                        (self.final_request_end_time - self.bridging_offset)
+                        / chunk_time
+                    )
+
+                    self.bridging_offset = round(
+                        (len(self.last_audio_input) - chunks_from_ms) * chunk_time
+                    )
+
+                    for i in range(chunks_from_ms, len(self.last_audio_input)):
+                        data.append(self.last_audio_input[i])
+
+                self.new_stream = False
+
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
+            chunk = self._buff.get()
+            self.audio_input.append(chunk)
+
+            if chunk is None:
+                return
+            data.append(chunk)
+            # Now consume whatever other data's still buffered.
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                    self.audio_input.append(chunk)
+
+                except queue.Empty:
+                    break
+
+            yield b"".join(data)
+
+
+def listen_print_loop(responses, stream):
+    """Iterates through server responses and prints them.
+    The responses passed is a generator that will block until a response
+    is provided by the server.
+    Each response may contain multiple results, and each result may contain
+    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+    print only the transcription for the top alternative of the top result.
+    In this case, responses are provided for interim results as well. If the
+    response is an interim one, print a line feed at the end of it, to allow
+    the next result to overwrite it, until the response is a final one. For the
+    final one, print a newline to preserve the finalized transcription.
+    """
+    x=0
+    for response in responses:
+
+        if get_current_time() - stream.start_time > STREAMING_LIMIT:
+            stream.start_time = get_current_time()
+            break
+
+        if not response.results:
+            continue
+
+        result = response.results[0]
+
+        if not result.alternatives:
+            continue
+
+        transcript = result.alternatives[0].transcript
+
+        result_seconds = 0
+        result_micros = 0
+
+        if result.result_end_time.seconds:
+            result_seconds = result.result_end_time.seconds
+
+        if result.result_end_time.microseconds:
+            result_micros = result.result_end_time.microseconds
+
+        stream.result_end_time = int((result_seconds * 1000) + (result_micros / 1000))
+
+        corrected_time = (
+            stream.result_end_time
+            - stream.bridging_offset
+            + (STREAMING_LIMIT * stream.restart_counter)
+        )
+        # Display interim results, but with a carriage return at the end of the
+        # line, so subsequent lines will overwrite them.
+
+        if result.is_final:
+
+            sys.stdout.write(GREEN)
+            sys.stdout.write("\033[K")
+            sys.stdout.write(str(corrected_time) + ": " + transcript + "\n")
+
+#EyeHear update OLED display with transcript text
+            displayText(transcript)
+
+            stream.is_final_end_time = stream.result_end_time
+            stream.last_transcript_was_final = True
+
+            # Exit recognition if any of the transcribed phrases could be
+            # one of our keywords.
+            if re.search(r"\b(exit|quit)\b", transcript, re.I):
+                sys.stdout.write(YELLOW)
+                sys.stdout.write("Exiting...\n")
+                stream.closed = True
+                break
+
+        else:
+            sys.stdout.write(RED)
+            sys.stdout.write("\033[K" + " " + SPINNER[x] )
+            if x>=7:
+             x=0
+            else:
+             x = x + 1 #todo replace with an iterator
+            sys.stdout.write(str(corrected_time) + ": " + transcript + "\r")
+#EyeHear overlay red text until it goes green
+            displayRedText(transcript)
+            stream.last_transcript_was_final = False
+
+
+def main():
+    """start bidirectional streaming from microphone input to speech API"""
+
+    client = speech.SpeechClient()
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=SAMPLE_RATE,
+        language_code="en-US",
+        max_alternatives=1,
+    )
+
+    streaming_config = speech.StreamingRecognitionConfig(
+        config=config, interim_results=True
+    )
+
+    mic_manager = ResumableMicrophoneStream(SAMPLE_RATE, CHUNK_SIZE)
+    print(mic_manager.chunk_size)
+    sys.stdout.write(YELLOW)
+    sys.stdout.write('\nListening, say "Quit" or "Exit" to stop.\n\n')
+    sys.stdout.write("End (ms)       Transcript Results/Status\n")
+    sys.stdout.write("=====================================================\n")
+
+    with mic_manager as stream:
+
+        while not stream.closed:
+            sys.stdout.write(YELLOW)
+            sys.stdout.write(
+                "\n" + str(STREAMING_LIMIT * stream.restart_counter) + ": NEW REQUEST\n"
+            )
+
+            stream.audio_input = []
+            audio_generator = stream.generator()
+
+            requests = (
+                speech.StreamingRecognizeRequest(audio_content=content)
+                for content in audio_generator
+            )
+
+            responses = client.streaming_recognize(streaming_config, requests)
+
+            # Now, put the transcription responses to use.
+            listen_print_loop(responses, stream)
+
+            if stream.result_end_time > 0:
+                stream.final_request_end_time = stream.is_final_end_time
+            stream.result_end_time = 0
+            stream.last_audio_input = []
+            stream.last_audio_input = stream.audio_input
+            stream.audio_input = []
+            stream.restart_counter = stream.restart_counter + 1
+
+            if not stream.last_transcript_was_final:
+                sys.stdout.write("\n")
+            stream.new_stream = True
+
+
+if __name__ == "__main__":
+
+    main()
+
+# [END speech_transcribe_infinite_streaming]
