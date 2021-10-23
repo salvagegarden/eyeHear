16f0a3ac64dfb3a0912b978260f6c75c9c70f841
Neng <xiad0001@ntu.edu.sg>
2021-10-23 09:49:47 +0800

    feat: display on ST7789

--- eyeHear/draw.py
+++ eyeHear/draw.py
@@ -1,107 +1,93 @@
 import textwrap
 import time
 
-# Import the SSD1306 module.
-import adafruit_ssd1306
-import busio
-# Import all board pins.
-from board import SCL, SDA
-
-##Saad salvage.garden 16sep: added OLED display support
-##Saad last update: 19sep - displayText for SST transcription output
-
-# SPDX-FileCopyrightText: Tony DiCola
-# SPDX-License-Identifier: CC0-1.0
-
-# Basic example of clearing and drawing pixels on a SSD1306 OLED display.
-# This example and library is meant to work with Adafruit CircuitPython API.
-
-
-# Create the I2C interface.
-i2c = busio.I2C(SCL, SDA)
-
+import Adafruit_GPIO as GPIO
+import Adafruit_GPIO.SPI as SPI
+import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
-# Setting some variables for our reset pin etc.
-# RESET_PIN = digitalio.DigitalInOut(board.D4)
-# Very important... This lets py-gaugette 'know' what pins to use in order to reset the display
+import ST7789 as TFT
 
-# Create the SSD1306 OLED class.
-# The first two parameters are the pixel width and pixel height.  Change these
-# to the right size for your display!
-display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
-# Alternatively you can change the I2C address of the device with an addr parameter:
-# display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x31)
+# Raspberry Pi pin configuration:
+RST = 25
+DC = 24
+LED = 27
+SPI_PORT = 0
+SPI_DEVICE = 0
+SPI_MODE = 0b11
+SPI_SPEED_HZ = 40000000
 
-# Clear the display.  Always call show after changing pixels to make the display
-# update visible!
-display.fill(0)
-display.show()
 
-# Audio recording parameters
-STREAMING_LIMIT = 240000  # 4 minutes
-SAMPLE_RATE = 16000
-CHUNK_SIZE = int(SAMPLE_RATE / 10)  # 100ms
+def expand2square(pil_img, background_color):
+    width, height = pil_img.size
+    if width == height:
+        return pil_img
+    elif width > height:
+        result = Image.new(pil_img.mode, (width, width), background_color)
+        result.paste(pil_img, (0, (width - height) // 2))
+        return result
+    else:
+        result = Image.new(pil_img.mode, (height, height), background_color)
+        result.paste(pil_img, ((height - width) // 2, 0))
+        return result
 
-RED = "\033[0;31m"
-GREEN = "\033[0;32m"
-YELLOW = "\033[0;33m"
-SPINNER = ["⠁", "⠂", "⠄", "⡀", "⢀", "⠠", "⠐", "⠈"]  # listener indicator = 8 frames
-x = 0
 
-# OLED Display INIT
+display = TFT.ST7789(
+    spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=SPI_SPEED_HZ),
+    mode=SPI_MODE,
+    rst=RST,
+    dc=DC,
+    led=LED,
+)
 
-# Create blank image for drawing.
-image = Image.new("1", (display.width, display.height))
+# Initialize display.
+display.begin()
+
+# Clear display.
+display.clear()
+
+# Analogue clock setting
+width = 240
+height = 240
+image = Image.new("RGB", (display.width, display.height), "BLACK")
 draw = ImageDraw.Draw(image)
 
-# Load a font in 2 different sizes.
-font = ImageFont.truetype(
-    "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 8
-)  # size 10 gives us 26 characters per screen
-tinyFont = ImageFont.truetype(
-    "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 8
-)  # size 8 gives us 30 characters per screen
-
-header = 10  # space for the first row / status bar / yellow region of OLED
-
-print("OLED Display test")
-display.fill(0)
-w_delta = display.width / 10
-h_delta = display.height / 10
-for i in range(4):
-    display.rect(0, 0, int(w_delta * i), int(h_delta * i), 1)
-display.show()
-time.sleep(0.1)
+# font setting
+# font = ImageFont.load_default()
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 20)
+header_font = ImageFont.truetype(
+    "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 22
+)
+header = header_font.getsize("0")[1] + 10
+line_width = int(width / font.getsize("0")[0])
+header_width = int(width / header_font.getsize("0")[0])
 
 
-print("OLED Display text test")
-try:
-    # Clear display.
-    display.fill(0)
-    display.show()
+def draw_rotated_text(image, text, position, angle, font, fill=(255, 255, 255)):
+    # print position
+    position = position[0], position[1]
+    # Get rendered font width and height.
+    draw = ImageDraw.Draw(image)
+    width, height = draw.textsize(text, font=font)
+    # Create a new image with transparent background to store the text.
+    textimage = Image.new("RGBA", (width, height), (0, 0, 0, 0))
+    # Render the text.
+    textdraw = ImageDraw.Draw(textimage)
+    textdraw.text((0, 0), text, font=font, fill=fill)
+    # Rotate the text image.
+    rotated = textimage.rotate(angle, expand=1)
+    # Paste the text into the image, using it as a mask for transparency.
+    image.paste(rotated, position, rotated)
 
-    # Draw the text
-    draw.text((0, 0), "Display activated!", font=tinyFont, fill=255)
-    display.image(image)
-    display.show()
-except Exception as e:
-    print("Display test failed ")
-    for i in range(11):
-        display.rect(0, 0, int(w_delta * i), int(h_delta * i), 1)
-    display.show()
-    print(e)
-
-time.sleep(0.1)
 
 def displayRedText(oledText):
     # overlay text on top row before it goes green
     draw = ImageDraw.Draw(image)
     draw.rectangle((0, 0, display.width, header), outline=0, fill=0)
 
-    draw.text((0, 0), oledText[-30:], font=tinyFont, fill=255)
+    draw.text((0, 0), oledText[-header_width:], font=header_font, fill=(220, 220, 0, 0))
     display.image(image)
-    display.show()
+    # display.show()
     # print("~") #debug oled
     time.sleep(0.1)
 
@@ -115,10 +101,10 @@ def displayText(oledText):
         (0, header, display.width - 1, display.height - 1), outline=1, fill=0
     )
     offset = header
-    lines = textwrap.wrap(oledText, width=30)
+    lines = textwrap.wrap(oledText, width=line_width)
     for line in lines:
-        draw.text((0, offset), line, font=font, fill=255)
+        draw.text((0, offset), line, font=font, fill=(0, 220, 255, 0))
         offset += font.getsize(line)[1]
     display.image(image)
-    display.show()
+    # display.show()
     time.sleep(0.1)
