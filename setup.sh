38031f993a997f66195bf89f793a9b9f7cd5c6d6
Neng <xiad0001@ntu.edu.sg>
2021-11-08 00:14:38 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/pi/Desktop
+cd /home/pi
 
 sudo apt-get update
 sudo apt-get install -y build-essential python-dev python-rpi.gpio python-spidev python-pip python-pil python-numpy raspberrypi-kernel-headers
@@ -8,7 +8,7 @@ pip3 install google-cloud-speech==2.9.3 PyAudio==0.2.11 Adafruit-GPIO==1.0.3
 
 git clone https://github.com/salvagegarden/eyeHear.git
 
-crontab -l | { cat; echo '@reboot GOOGLE_APPLICATION_CREDENTIALS="/home/pi/engineering-good-1569744069426-f14580c084fd.json" python3 /home/pi/Desktop/eyeHear/main.py  >> /home/pi/eyeHear.log 2>&1'; } | crontab -
+crontab -l | { cat; echo '@reboot GOOGLE_APPLICATION_CREDENTIALS="/home/pi/engineering-good-1569744069426-f14580c084fd.json" python3 /home/pi/eyeHear/eyeHear/main.py  >> /home/pi/eyeHear.log 2>&1'; } | crontab -
 
 # Setup i2smic driver
 sudo pip3 install adafruit-python-shell
